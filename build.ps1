0c7cfde3920debc3046592a443870973c6783e8e
Blaquewithaq <blaquewithaq@gmail.com>
2024-01-23 09:50:51 -0600

    Add build configuration

---
+++ build.ps1
@@ -0,0 +1,84 @@
+# Check if environment variables are set
+if (!$Env:PROJECTM4_DIR) {
+    Write-Host "PROJECTM4_DIR environment variable not set"
+    exit 1
+}
+
+if (!$Env:VCPKG_INSTALLATION_ROOT) {
+    Write-Host "VCPKG_INSTALLATION_ROOT environment variable not set"
+    exit 1
+}
+
+$AUTO = "false"
+
+# Skip prompt if --auto is passed
+if ($args[0] -eq "--auto") {
+    $AUTO = "true"
+}
+
+# Set current directory
+$ROOT = Get-Location
+$BUILD = "$ROOT\build"
+$DIST = "$ROOT\dist"
+
+# Print directories
+Write-Host "Current directory: $ROOT"
+Write-Host "ProjectM directory: $Env:PROJECTM4_DIR"
+
+# Clean up previous build files
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue -Path "$BUILD"
+Remove-Item -Recurse -Force -ErrorAction SilentlyContinue -Path "$DIST"
+New-Item -Path "$BUILD" -ItemType Directory | Out-Null
+New-Item -Path "$DIST" -ItemType Directory | Out-Null
+
+# Configure build
+cmake `
+    -G "Visual Studio 17 2022" `
+    -A "X64" `
+    -S "$ROOT" `
+    -B "$BUILD" `
+    -DCMAKE_TOOLCHAIN_FILE="${Env:VCPKG_INSTALLATION_ROOT}/scripts/buildsystems/vcpkg.cmake" `
+    -DVCPKG_TARGET_TRIPLET=x64-windows `
+    -DCMAKE_MSVC_RUNTIME_LIBRARY="MultiThreaded$<$<CONFIG:Debug>:Debug>DLL" `
+    -DCMAKE_VERBOSE_MAKEFILE=YES `
+    -DCMAKE_PREFIX_PATH="${Env:PROJECTM4_DIR}"
+
+# Build
+cmake --build "$BUILD" --config "Release" --parallel
+
+# Move to dist
+Copy-Item -Path "$BUILD\Release\gstprojectm.dll" -Destination "$DIST\gstprojectm.dll" -Force
+
+if ($AUTO -eq "true") {
+    exit 0
+}
+
+# Ask to install
+Write-Host "Install to gstreamer plugins? [Y/n]"
+$answer = Read-Host
+
+if ($answer -ne "N" -and $answer -ne "n") {
+    # Use environment variable if set, otherwise use default
+    if ($Env:GST_PLUGIN_PATH) {
+        $GST_PLUGINS_DIR = $Env:GST_PLUGIN_PATH
+    } else {
+        $GST_PLUGINS_DIR = "$Env:USERPROFILE\.gstreamer\1.0\plugins"
+        # Set environment variable
+        [Environment]::SetEnvironmentVariable("GST_PLUGIN_PATH", $GST_PLUGINS_DIR, "User")
+    }
+
+    # Create the destination directory if it doesn't exist
+    New-Item -Path "$GST_PLUGINS_DIR" -ItemType Directory | Out-Null
+
+    # Move the file to the destination, overwriting if it exists
+    Copy-Item -Path "$DIST\gstprojectm.dll" -Destination "$GST_PLUGINS_DIR\gstprojectm.dll" -Force
+
+    # Print example command
+    Write-Host
+    Write-Host "Done! Here's an example command:"
+    Write-Host 'gst-launch-1.0 audiotestsrc ! queue ! audioconvert ! projectm ! "video/x-raw,width=512,height=512,framerate=60/1" ! videoconvert ! xvimagesink sync=false'
+} else {
+    Write-Host
+    Write-Host "Done!"
+    Write-Host "You can install the plugin manually by moving <dist\libgstprojectm.dll> to <$GST_PLUGINS_DIR\libgstprojectm.dll>"
+}
---
+++ src/plugin.h
@@ -0,0 +1,43 @@
+/*
+ * GStreamer
+ * Copyright (C) 2024 AnomieVision <>
+ */
+
+#ifndef __GST_PLUGIN_PROJECTM_H__
+#define __GST_PLUGIN_PROJECTM_H__
+
+#include <gst/gst.h>
+#include <gst/pbutils/gstaudiovisualizer.h>
+
+G_BEGIN_DECLS
+
+#define GST_TYPE_PLUGIN_PROJECTM (gst_plugin_projectm_get_type())
+G_DECLARE_FINAL_TYPE(GstPluginProjectM, gst_plugin_projectm, GST,
+                     PLUGIN_PROJECTM, GstElement)
+
+struct _GstPluginProjectM {
+  GstAudioVisualizer element;
+  GstPad *sinkpad, *srcpad;
+  gboolean silent;
+  gchar *preset;
+};
+
+struct _GstPluginProjectMClass {
+  GstAudioVisualizerClass parent_class;
+};
+
+static void gst_plugin_projectm_set_property(GObject *object, guint prop_id,
+                                             const GValue *value,
+                                             GParamSpec *pspec);
+static void gst_plugin_projectm_get_property(GObject *object, guint prop_id,
+                                             GValue *value, GParamSpec *pspec);
+
+static void gst_plugin_projectm_init(GstPluginProjectM *plugin);
+
+static void gst_plugin_projectm_finalize(GObject *object);
+
+static void gst_plugin_projectm_class_init(GstPluginProjectMClass *klass);
+
+G_END_DECLS
+
+#endif /* __GST_PLUGIN_PROJECTM_H__ */
