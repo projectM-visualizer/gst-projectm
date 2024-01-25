9d44c37ead93426586c055e19bccd8d1ac7244a1
Blaquewithaq <blaquewithaq@gmail.com>
2024-01-25 16:37:27 -0600

    Fix setup script and workflows

--- setup.sh
+++ setup.sh
@@ -6,61 +6,81 @@ set -e
 
 # Install packages based on the selected package manager: apt, pacman, brew
 install_packages() {
-    local package_manager=$1
-    local package_list=("${!2}")
+    local AUTO=$1
+    local PACKAGE_MANAGER=$2
+    local PACKAGE_LIST=("${!3}")
     
-    case $package_manager in
+    case $PACKAGE_MANAGER in
         apt)
             sudo apt update
-            sudo apt install "${package_list[@]}"
+            
+            if [ $AUTO = true ]; then
+                sudo apt install -y "${PACKAGE_LIST[@]}"
+            else
+                sudo apt install "${PACKAGE_LIST[@]}"
+            fi
         ;;
         pacman)
             sudo pacman -Syyu
-            sudo pacman -S "${package_list[@]}"
+            
+            if [ $AUTO = true ]; then
+                sudo pacman -Sy "${PACKAGE_LIST[@]}"
+            else
+                sudo pacman -S "${PACKAGE_LIST[@]}"
+            fi
         ;;
         brew)
             brew update
-            brew install "${package_list[@]}"
+            
+            if [ $AUTO = true ]; then
+                brew install "${PACKAGE_LIST[@]}" || true
+            else
+                brew install "${PACKAGE_LIST[@]}"
+            fi
         ;;
     esac
 }
 
 # Prompt user to install dependencies, (and choose a package manager, if multiple are available)
 prompt_install_dependencies() {
-    if [ $1 = false ]; then
+    local AUTO=$1
+    
+    if [ $AUTO = false ]; then
         echo
         echo -n "Install dependencies? (Y/n): "
-        read -r install_deps
+        read -r INSTALL_DEPS
     else
-        install_deps="n"
+        INSTALL_DEPS="Y"
     fi
     
-    if [[ "$install_deps" != "N" && "$install_deps" != "n" ]]; then
+    if [[ "$INSTALL_DEPS" != "N" && "$INSTALL_DEPS" != "n" ]]; then
         # Check for available package managers
-        available_package_managers=()
+        AVAILABLE_PACKAGE_MANAGERS=()
         
-        if command -v apt &>/dev/null; then
-            available_package_managers+=("apt")
-        fi
-        
-        if command -v pacman &>/dev/null; then
-            available_package_managers+=("pacman")
-        fi
-        
-        if command -v brew &>/dev/null; then
-            available_package_managers+=("brew")
+        if [ "$(uname)" == "Darwin" ]; then
+            if command -v brew &>/dev/null; then
+                AVAILABLE_PACKAGE_MANAGERS+=("brew")
+            fi
+        elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+            if command -v apt &>/dev/null; then
+                AVAILABLE_PACKAGE_MANAGERS+=("apt")
+            fi
+            
+            if command -v pacman &>/dev/null; then
+                AVAILABLE_PACKAGE_MANAGERS+=("pacman")
+            fi
         fi
         
         # Prompt user to choose a package manager
-        if [ ${#available_package_managers[@]} -eq 0 ]; then
+        if [ ${#AVAILABLE_PACKAGE_MANAGERS[@]} -eq 0 ]; then
             echo "No supported package managers found."
             exit 1
-            elif [ ${#available_package_managers[@]} -eq 1 ] || [ $AUTO = true ]; then
-            selected_package_manager=${available_package_managers[0]}
+            elif [ ${#AVAILABLE_PACKAGE_MANAGERS[@]} -eq 1 ] || [ $AUTO = true ]; then
+            SELECTED_PACKAGE_MANAGER=${AVAILABLE_PACKAGE_MANAGERS[0]}
         else
             echo -n "Multiple package managers found. Please choose one:"
-            select selected_package_manager in "${available_package_managers[@]}"; do
-                if [ -n "$selected_package_manager" ]; then
+            select SELECTED_PACKAGE_MANAGER in "${AVAILABLE_PACKAGE_MANAGERS[@]}"; do
+                if [ -n "$SELECTED_PACKAGE_MANAGER" ]; then
                     break
                 else
                     echo "Invalid selection. Please choose a number."
@@ -69,21 +89,20 @@ prompt_install_dependencies() {
         fi
         
         # Install packages based on the selected package manager
-        case $selected_package_manager in
+        case $SELECTED_PACKAGE_MANAGER in
             apt)
-                package_list=("ninja-build" "mesa-common-dev" "build-essential" "git" "cmake" "pkg-config" "libgstreamer1.0-dev" "libgstreamer-gl1.0-0" "libgstreamer-plugins-base1.0-dev")
-                install_packages "$selected_package_manager" package_list[@]
+                PACKAGE_LIST=("git" "cmake" "ninja-build" "pkg-config" "build-essential" "libgl1-mesa-dev" "mesa-common-dev" "libgstreamer1.0-dev" "libgstreamer-gl1.0-0" "libgstreamer-plugins-base1.0-dev" "libunwind-dev")
+                install_packages $AUTO "$SELECTED_PACKAGE_MANAGER" PACKAGE_LIST[@]
             ;;
             pacman)
-                package_list=("base-devel" "ninja" "cmake" "mesa" "gst-plugins-base-libs")
-                install_packages "$selected_package_manager" package_list[@]
+                PACKAGE_LIST=("git" "cmake" "ninja" "pkgconf" "base-devel"  "mesa" "gst-plugins-base-libs")
+                install_packages $AUTO "$SELECTED_PACKAGE_MANAGER" PACKAGE_LIST[@]
             ;;
             brew)
-                package_list=("git" "ninja" "cmake" "gstreamer")
-                install_packages "$selected_package_manager" package_list[@]
+                PACKAGE_LIST=("git" "cmake" "ninja" "pkg-config" "gstreamer")
+                install_packages $AUTO "$SELECTED_PACKAGE_MANAGER" PACKAGE_LIST[@]
             ;;
         esac
-        
     else
         echo
         echo "Skipping dependency installation."
@@ -92,19 +111,21 @@ prompt_install_dependencies() {
 
 # ------------
 # FUNCTIONS: FIXES
-
+# /usr/local/Cellar/gstreamer/1.22.8_2/include/gstreamer-1.0/gst/gl
 # Fix missing "#include <gst/gl/gstglconfig.h>"
 fix_missing_gstglconfig_h() {
-    if [ ! -f "/usr/include/gstreamer-1.0/gst/gl/gstglconfig.h" ]; then
+    fix() {
+        local FILEPATH=$1
+        
         echo
         echo "Fixing missing gst/gl/gstglconfig.h..."
         echo
         
         # Create missing header file
-        sudo touch /usr/include/gstreamer-1.0/gst/gl/gstglconfig.h
+        sudo touch $FILEPATH
         
         # Add contents to header file
-        sudo tee /usr/include/gstreamer-1.0/gst/gl/gstglconfig.h <<EOF
+        sudo tee $FILEPATH <<EOF
 /* gstglconfig.h */
 
 #ifndef __GST_GL_CONFIG_H__
@@ -154,22 +175,44 @@ G_END_DECLS
 
 #endif  /* __GST_GL_CONFIG_H__ */
 EOF
+    }
+    if [ "$(uname)" == "Darwin" ]; then
+        if [ -d "/usr/local/Cellar/gstreamer/1.22.8_2/include/gstreamer-1.0/gst/gl" ] || [ ! -f "/usr/local/Cellar/gstreamer/1.22.8_2/include/gstreamer-1.0/gst/gl/gstglconfig.h" ]; then
+            DIRECTORY="/usr/local/Cellar/gstreamer/1.22.8_2/include/gstreamer-1.0/gst/gl"
+            FILE="/usr/local/Cellar/gstreamer/1.22.8_2/include/gstreamer-1.0/gst/gl/gstglconfig.h"
+        fi
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        if [ -d "/usr/include/gstreamer-1.0/gst/gl" ] || [ ! -f "/usr/include/gstreamer-1.0/gst/gl/gstglconfig.h" ]; then
+            DIRECTORY="/usr/include/gstreamer-1.0/gst/gl"
+            FILE="/usr/include/gstreamer-1.0/gst/gl/gstglconfig.h"
+        fi
+    fi
+
+    if [ -d $DIRECTORY ] || [ ! -f $FILE ]; then
+        fix $FILE
+    elif [ -f $FILE ]; then
+        echo
+        echo "gst/gl/gstglconfig.h already exists."
+    else
+        echo
+        echo 'Unable to fix missing "#include <gst/gl/gstglconfig.h>".'
     fi
 }
 
 # Prompt user to apply fixes
 prompt_apply_fixes() {
-    if [ $1 = false ]; then
+    local AUTO=$1
+    if [ $AUTO = false ]; then
         echo
         echo "The following fixes are available:"
         echo "  - Missing gst/gl/gstglconfig.h"
         echo -n "Apply fixes? (Y/n): "
-        read -r apply_fixes
+        read -r APPLY_FIXES
     else
-        install_deps="n"
+        APPLY_FIXES="Y"
     fi
     
-    if [[ "$apply_fixes" != "N" && "$apply_fixes" != "n" ]]; then
+    if [[ "$APPLY_FIXES" != "N" && "$APPLY_FIXES" != "n" ]]; then
         fix_missing_gstglconfig_h
     else
         echo
