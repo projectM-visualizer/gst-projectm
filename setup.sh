ed8a94f3e37cada66bf4150136586a660b24fcdc
Blaquewithaq <blaquewithaq@gmail.com>
2024-01-28 20:45:23 -0600

    Updated setup.sh to check for xcode on OSX

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 # ------------
-# FUNCTIONS: PACKAGES
+# FUNCTIONS
 
 # Install packages based on the selected package manager: apt, pacman, brew
 install_packages() {
@@ -69,6 +69,10 @@ prompt_install_dependencies() {
             if command -v pacman &>/dev/null; then
                 AVAILABLE_PACKAGE_MANAGERS+=("pacman")
             fi
+
+            # if command -v yum &>/dev/null; then
+            #     AVAILABLE_PACKAGE_MANAGERS+=("yum")
+            # fi
         fi
         
         # Prompt user to choose a package manager
@@ -98,6 +102,10 @@ prompt_install_dependencies() {
                 PACKAGE_LIST=("git" "cmake" "ninja" "pkgconf" "base-devel"  "mesa" "gst-plugins-base-libs" "gst-libav")
                 install_packages $AUTO "$SELECTED_PACKAGE_MANAGER" PACKAGE_LIST[@]
             ;;
+            # yum)
+            #     PACKAGE_LIST=()
+            #     install_packages $AUTO "$SELECTED_PACKAGE_MANAGER" PACKAGE_LIST[@]
+            # ;;
             brew)
                 PACKAGE_LIST=("git" "cmake" "ninja" "pkg-config" "gstreamer")
                 install_packages $AUTO "$SELECTED_PACKAGE_MANAGER" PACKAGE_LIST[@]
@@ -109,93 +117,20 @@ prompt_install_dependencies() {
     fi
 }
 
-# ------------
-# FUNCTIONS: FIXES
-# /usr/local/Cellar/gstreamer/1.22.8_2/include/gstreamer-1.0/gst/gl
-# Fix missing "#include <gst/gl/gstglconfig.h>"
-fix_missing_gstglconfig_h() {
-    fix() {
-        local FILEPATH=$1
-        
-        echo
-        echo "Fixing missing gst/gl/gstglconfig.h..."
-        echo
-        
-        # Create missing header file
-        sudo touch $FILEPATH
-        
-        # Add contents to header file
-        sudo tee $FILEPATH <<EOF
-/* gstglconfig.h */
-
-#ifndef __GST_GL_CONFIG_H__
-#define __GST_GL_CONFIG_H__
-
-#include <gst/gst.h>
-
-G_BEGIN_DECLS
-
-
-#define GST_GL_HAVE_OPENGL 1
-#define GST_GL_HAVE_GLES2 1
-#define GST_GL_HAVE_GLES3 1
-#define GST_GL_HAVE_GLES3EXT3_H 1
-
-#define GST_GL_HAVE_WINDOW_X11 1
-#define GST_GL_HAVE_WINDOW_COCOA 0
-#define GST_GL_HAVE_WINDOW_WIN32 0
-#define GST_GL_HAVE_WINDOW_WINRT 0
-#define GST_GL_HAVE_WINDOW_WAYLAND 1
-#define GST_GL_HAVE_WINDOW_ANDROID 0
-#define GST_GL_HAVE_WINDOW_DISPMANX 0
-#define GST_GL_HAVE_WINDOW_EAGL 0
-#define GST_GL_HAVE_WINDOW_VIV_FB 0
-#define GST_GL_HAVE_WINDOW_GBM 1
-
-#define GST_GL_HAVE_PLATFORM_EGL 1
-#define GST_GL_HAVE_PLATFORM_GLX 1
-#define GST_GL_HAVE_PLATFORM_WGL 0
-#define GST_GL_HAVE_PLATFORM_CGL 0
-#define GST_GL_HAVE_PLATFORM_EAGL 0
-
-#define GST_GL_HAVE_DMABUF 1
-#define GST_GL_HAVE_VIV_DIRECTVIV 0
-
-#define GST_GL_HAVE_GLEGLIMAGEOES 1
-#define GST_GL_HAVE_GLCHAR 1
-#define GST_GL_HAVE_GLSIZEIPTR 1
-#define GST_GL_HAVE_GLINTPTR 1
-#define GST_GL_HAVE_GLSYNC 1
-#define GST_GL_HAVE_GLUINT64 1
-#define GST_GL_HAVE_GLINT64 1
-#define GST_GL_HAVE_EGLATTRIB 1
-#define GST_GL_HAVE_EGLUINT64KHR 1
-
-G_END_DECLS
-
-#endif  /* __GST_GL_CONFIG_H__ */
-EOF
-    }
-    if [ "$(uname)" == "Darwin" ]; then
-        if [ -d "/usr/local/Cellar/gstreamer/1.22.8_2/include/gstreamer-1.0/gst/gl" ] || [ ! -f "/usr/local/Cellar/gstreamer/1.22.8_2/include/gstreamer-1.0/gst/gl/gstglconfig.h" ]; then
-            DIRECTORY="/usr/local/Cellar/gstreamer/1.22.8_2/include/gstreamer-1.0/gst/gl"
-            FILE="/usr/local/Cellar/gstreamer/1.22.8_2/include/gstreamer-1.0/gst/gl/gstglconfig.h"
-        fi
-    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        if [ -d "/usr/include/gstreamer-1.0/gst/gl" ] || [ ! -f "/usr/include/gstreamer-1.0/gst/gl/gstglconfig.h" ]; then
-            DIRECTORY="/usr/include/gstreamer-1.0/gst/gl"
-            FILE="/usr/include/gstreamer-1.0/gst/gl/gstglconfig.h"
-        fi
+# Check if Xcode command-line tools are installed and properly set up
+check_xcode() {
+    # Check if Xcode command-line tools are installed
+    if ! xcode-select -p &>/dev/null; then
+        echo "Xcode command-line tools are not installed."
+        echo "Please install them by running: xcode-select --install"
+        exit 1
     fi
 
-    if [ -d $DIRECTORY ] || [ ! -f $FILE ]; then
-        fix $FILE
-    elif [ -f $FILE ]; then
-        echo
-        echo "gst/gl/gstglconfig.h already exists."
-    else
-        echo
-        echo 'Unable to fix missing "#include <gst/gl/gstglconfig.h>".'
+    # Check if Xcode license agreement is accepted
+    if ! xcodebuild -version &>/dev/null; then
+        echo "Xcode license agreement is not accepted."
+        echo "Please accept the license agreement by running: sudo xcodebuild -license accept"
+        exit 1
     fi
 }
 
@@ -210,3 +145,7 @@ if [ "$1" = "--auto" ] ; then
 fi
 
 prompt_install_dependencies $AUTO
+
+if [ "$(uname)" == "Darwin" ]; then
+    check_xcode
+fi
