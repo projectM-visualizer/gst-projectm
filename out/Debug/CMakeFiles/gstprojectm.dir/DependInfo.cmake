ade8e2d27373865cf4afdf2db189c8d2fa0e6107
Mischa Spiegelmock <me@mish.dev>
2025-04-04 20:34:00 -0700

    Run clang-format

---
+++ out/Debug/CMakeFiles/gstprojectm.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/cyber/dev/pm/gst-projectm/src/caps.c" "CMakeFiles/gstprojectm.dir/src/caps.c.o" "gcc" "CMakeFiles/gstprojectm.dir/src/caps.c.o.d"
+  "/home/cyber/dev/pm/gst-projectm/src/debug.c" "CMakeFiles/gstprojectm.dir/src/debug.c.o" "gcc" "CMakeFiles/gstprojectm.dir/src/debug.c.o.d"
+  "/home/cyber/dev/pm/gst-projectm/src/gstglbaseaudiovisualizer.c" "CMakeFiles/gstprojectm.dir/src/gstglbaseaudiovisualizer.c.o" "gcc" "CMakeFiles/gstprojectm.dir/src/gstglbaseaudiovisualizer.c.o.d"
+  "/home/cyber/dev/pm/gst-projectm/src/plugin.c" "CMakeFiles/gstprojectm.dir/src/plugin.c.o" "gcc" "CMakeFiles/gstprojectm.dir/src/plugin.c.o.d"
+  "/home/cyber/dev/pm/gst-projectm/src/projectm.c" "CMakeFiles/gstprojectm.dir/src/projectm.c.o" "gcc" "CMakeFiles/gstprojectm.dir/src/projectm.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
